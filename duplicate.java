6c6344839c97f69f9b0c7c506b9f7734568abca7
Karthik <kmadival1@gmail.com>
2022-07-19 23:38:36 +0530

    Stash

--- duplicate.java
+++ duplicate.java
@@ -3,11 +3,15 @@ package maven_pr;
 public class duplicate {
 
 	public static void main(String[] args) {
-
 		System.out.println("dummy");
-		
 	}
 	public void Stashcheck(){
 	}
 	
+
+
+	public void testone() {
+		System.out.println("Test one");
+	}
+
 }
