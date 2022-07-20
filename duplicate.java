5e20f2af52b4d6f438800d4dfa7dba79b153b58d
Karthik <kmadival1@gmail.com>
2022-07-20 10:54:09 +0530

    test three

--- duplicate.java
+++ duplicate.java
@@ -1,17 +1,6 @@
 package maven_pr;
 
 public class duplicate {
-
-	public static void main(String[] args) {
-		System.out.println("dummy");
-	}
-	public void Stashcheck(){
-	}
-	
-
-
-	public void testone() {
-		System.out.println("Test one");
-	}
+	public void TestThree() {}
 
 }
