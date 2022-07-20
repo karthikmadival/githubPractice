22ad8f7f1b77d25a96f750258c04894174b81df1
Karthik <kmadival1@gmail.com>
2022-07-20 10:45:37 +0530

    added 4th test

--- duplicate.java
+++ duplicate.java
@@ -2,16 +2,7 @@ package maven_pr;
 
 public class duplicate {
 
-	public static void main(String[] args) {
-		System.out.println("dummy");
-	}
-	public void Stashcheck(){
-	}
+	public void Testfour(){}
 	
-
-
-	public void testone() {
-		System.out.println("Test one");
-	}
-
+	
 }
